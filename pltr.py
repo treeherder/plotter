047470fce07c2c65637091c6570e84af5e2deb96
Brendan Reddy-Best <brendan@techlabeducation.com>
2013-11-28 02:44:41 -0800

    initial features for testing
    template.txt is ready to be tested as a subdirectory index.html

---
+++ pltr.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+import errno
+
+
+parser = argparse.ArgumentParser(description='make static html with consitent styling')
+parser.add_argument('--jmtrn', dest='jmtrn')
+parser.add_argument('--cols', dest='cols')
+parser.add_argument('--rows', dest='rows')
+parser.add_argument('--cells', dest='cells')
+parser.add_argument('--dir', dest='dir')
+
+args = parser.parse_args()
+dir = args.dir
+
+root = '/var/www/' #where html files are served from
+subdir = root+dir
+
+def chk_pth(pth):
+  try:
+    os.makedirs(pth)
+  except OSError as expt:
+    if expt.errno != errno.EEXIST:
+      raise
+						
+						
+def gen_static(loc):  # use this method to make some html
+	with open('template.txt') as r:  #this is a more or less complete page
+		with open('{0}/index.html'.format(loc), 'a+') as w:  # for test purposes
+			for line in r:
+				w.write(line)
+	r.close()
+	w.close()
+
+	
+	
+chk_pth(path)
+gen_static(subdir)
---
+++ readme.md
@@ -0,0 +1,10 @@
+plotter, a static html generator to keep the site's theme and layout consistent.
+I decided to build this tool as an aid to my first attempts at building a website, after realizing how tedious it can be to generate all of this static content by hand. 
+Since I'm unfamiliar with PHP, and am launching this site on a Raspberry Pi, anyway, I figured I would just take a stab at it in python, for educational purposes.
+
+	To Do:
+		- compile list of elements for args
+		- make a template for each element
+		- ???
+		- profit
+
