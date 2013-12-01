e1f3e1f09a5ceeb120a2d4d8e50159baa7c86870
treeherder <b.d.reddybest@gmail.com>
2013-11-30 23:48:07 -0800

    really worked out some structure stuff

--- pltr.py
+++ pltr.py
@@ -3,21 +3,25 @@ import os
 import errno
 from xml.dom.minidom import parse
 
-parser = argparse.ArgumentParser(description='make static html with consitent styling')
-parser.add_argument('--jmtrn', dest='jmtrn')
-parser.add_argument('--cols', dest='cols')
-parser.add_argument('--rows', dest='rows')
-parser.add_argument('--cells', dest='cells')
-parser.add_argument('--dir', dest='dir')
+parser = argparse.ArgumentParser(description='make static html with consistent styling')
+parser.add_argument('--wiki', dest='wiki')
+parser.add_argument('--blog', dest='blog')
+parser.add_argument('--general', dest='general')
 
-args = parser.parse_args()
+parser.add_argument('--dir', dest='dir')
+parser.add_argument('--rd' dest='rd')
+
+
+
+
+args = parser.parse_args() 
+#this is the list of relevant 
 dir = args.dir
-nav = args.nav
-jmtrn - args.jmtrn
 
 
 root = '/var/www/' #where html files are served from
 subdir = root+dir #the name of the html page we want to create
+
 
 def chk_pth(pth):
   try:
@@ -28,14 +32,19 @@ def chk_pth(pth):
 						
 						
 def gen_static(loc):  # use this method to make some html
-  with open('template.txt') as r:  #this is a more or less complete page
+  with open('{0}'.format(args.rd)) as r:  #this is a more or less complete page
     with open('{0}/index.html'.format(loc), 'a+') as w:  # for test purposes
       for line in r:
         w.write(line)
   r.close()
   w.close()
 
-	
-	
-chk_pth(subdir)
-gen_static(subdir)
+def template():
+  if args.wiki != False:
+    make_page()
+  elif args.blog != False:
+  elif args.general != False:
+
+def make_page():   # do stuff
+  chk_pth(subdir)
+  gen_static(subdir)
